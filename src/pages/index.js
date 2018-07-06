39ccd02ecd94f14ab94f6eeddbd4ed6de7029b28
Sander Visser <info@sanderfish.nl>
2018-07-06 16:46:06 +0200

    Fix Bookinglayer links

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,19 @@
-import React from 'react'
-import Link from 'gatsby-link'
+import React from 'react';
+import Link from 'gatsby-link';
 
-const IndexPage = () =>
+const IndexPage = () => (
   <div>
     <div className="pv2">
       <h4># Hey, I'm Sander.</h4>
       <p>
-        I'm an indie maker and freelance full stack developer. I like working on digital products, web applications and websites. I'm from a small town in the Netherlands, but am traveling and working across Europe by plane, train and van. I tweet <a href="https://twitter.com/fishsander">@fishsander</a> and (sometimes) write <a href="https://medium.com/@sandervisser">@sandervisser</a>. Some of the projects I've worked on are down here:</p>
+        I'm an indie maker and freelance full stack developer. I like working on
+        digital products, web applications and websites. I'm from a small town
+        in the Netherlands, but am traveling and working across Europe by plane,
+        train and van. I tweet{' '}
+        <a href="https://twitter.com/fishsander">@fishsander</a> and (sometimes)
+        write <a href="https://medium.com/@sandervisser">@sandervisser</a>. Some
+        of the projects I've worked on are down here:
+      </p>
 
       <ul className="w-80">
         <li className="pv2">
@@ -14,7 +21,8 @@ const IndexPage = () =>
             <a href="https://checkoutpage.co">Checkout Page</a>
           </div>
           <div className="pv1">
-            Founder / Helping business owners and creators receive money online without writing code.
+            Founder / Helping business owners and creators receive money online
+            without writing code.
           </div>
           <div className="pv1">
             NodeJS, Koa, ReactJS, Redux, Tailwind CSS, Stripe Connect, Heroku
@@ -22,104 +30,109 @@ const IndexPage = () =>
         </li>
         <li className="pv2">
           <div className="pv1">
-            <a href="https://bookinglayer.com/">Bookinglayer Mobile</a><br />
+            <a href="https://www.bookinglayer.com/en/">Bookinglayer Mobile</a>
+            <br />
           </div>
           <div className="pv1">
-            Freelance / design and development of mobile app for booking management (with <a href="http://stay-bold.com">Stay Bold</a>).<br />
+            Freelance / design and development of mobile app for booking
+            management (with <a href="http://stay-bold.com">Stay Bold</a>).<br />
           </div>
-          <div className="pv1">
-            React Native (cross platform), Redux
-          </div>
+          <div className="pv1">React Native (cross platform), Redux</div>
         </li>
         <li className="pv2">
           <div className="pv1">
-            <a href="https://bookinglayer.com/">Bookinglayer</a><br />
+            <a href="https://www.bookinglayer.com/en/">Bookinglayer</a>
+            <br />
           </div>
           <div className="pv1">
-            Freelance / Design and development of marketing website (with <a href="http://stay-bold.com">Stay Bold</a>).<br />
+            Freelance / Design and development of marketing website (with{' '}
+            <a href="http://stay-bold.com">Stay Bold</a>).<br />
           </div>
-          <div className="pv1">
-            Jekyll, Markdown, Sass/Foundation
-          </div>
+          <div className="pv1">Jekyll, Markdown, Sass/Foundation</div>
         </li>
         <li className="pv2">
           <div className="pv1">
-            <a href="https://convertcalculator.co">ConvertCalculator Checkout</a><br />
+            <a href="https://convertcalculator.co">
+              ConvertCalculator Checkout
+            </a>
+            <br />
           </div>
           <div className="pv1">
-            Freelance / Adding Stripe Connect checkout functionality to existing application.<br />
+            Freelance / Adding Stripe Connect checkout functionality to existing
+            application.<br />
           </div>
-          <div className="pv1">
-            Meteor, React, Stripe Connect
-          </div>
+          <div className="pv1">Meteor, React, Stripe Connect</div>
         </li>
         <li className="pv2">
           <div className="pv1">
-            <a href="http://wordeys.com">Wordeys</a><br />
+            <a href="http://wordeys.com">Wordeys</a>
+            <br />
           </div>
           <div className="pv1">
             Founder / Helping people practise vocabulary online.<br />
           </div>
-          <div className="pv1">
-            Ruby on Rails, Vanilla JS, Sass, Heroku
-          </div>
+          <div className="pv1">Ruby on Rails, Vanilla JS, Sass, Heroku</div>
         </li>
         <li className="pv2">
           <div className="pv1">
-            <a href="http://bramsparis.com">Brams Paris</a><br />
+            <a href="http://bramsparis.com">Brams Paris</a>
+            <br />
           </div>
           <div className="pv1">
-            Freelance / e-commerce design and development (with <a href="http://parlor.nl">Parlor</a>).<br />
+            Freelance / e-commerce design and development (with{' '}
+            <a href="http://parlor.nl">Parlor</a>).<br />
           </div>
-          <div className="pv1">
-            Craft CMS, Twig, Sass/Foundation, jQuery
-          </div>
+          <div className="pv1">Craft CMS, Twig, Sass/Foundation, jQuery</div>
         </li>
         <li className="pv2">
           <div className="pv1">
-            <a href="http://cribber.nl">Cribber</a><br />
+            <a href="http://cribber.nl">Cribber</a>
+            <br />
           </div>
           <div className="pv1">
-            Freelance / designing and developing a social platform for stundent homes in the Netherlands.<br />
+            Freelance / designing and developing a social platform for stundent
+            homes in the Netherlands.<br />
           </div>
-          <div className="pv1">
-            PHP, Sass, jQuery
-          </div>
+          <div className="pv1">PHP, Sass, jQuery</div>
         </li>
         <li className="pv2">
           <div className="pv1">
-            <a href="http://martmakelaardij.nl">Mart Makelaardij</a><br />
+            <a href="http://martmakelaardij.nl">Mart Makelaardij</a>
+            <br />
           </div>
           <div className="pv1">
             Freelance / design and development of real estate agent website.<br />
           </div>
-          <div className="pv1">
-            Craft CMS, Twig, Sass/Foundation, jQuery
-          </div>
+          <div className="pv1">Craft CMS, Twig, Sass/Foundation, jQuery</div>
         </li>
         <li className="pv2">
           <div className="pv1">
-            <a href="http://fishhq.nl/pickle-app/">Pickle</a><br />
+            <a href="http://fishhq.nl/pickle-app/">Pickle</a>
+            <br />
           </div>
           <div className="pv1">
             Bachelor course / Interaction and visual design as student project.
           </div>
-          <div className="pv1">
-            Sketch, Framer
-          </div>
+          <div className="pv1">Sketch, Framer</div>
         </li>
       </ul>
     </div>
 
     <div className="pv2">
       <h4># Stay updated.</h4>
-      <p><a href="http://eepurl.com/cz6K85">Sign up to my email list</a> to be the first to know about my writing and the products I launch.</p>
+      <p>
+        <a href="http://eepurl.com/cz6K85">Sign up to my email list</a> to be
+        the first to know about my writing and the products I launch.
+      </p>
     </div>
 
     <div className="pv2">
       <h4># Tools and technologies</h4>
       <div>
-        <p>The tools and technologies I use depend on the project, but my currently preferred stack contains:</p>
+        <p>
+          The tools and technologies I use depend on the project, but my
+          currently preferred stack contains:
+        </p>
 
         <ul>
           <li>Pencil & paper</li>
@@ -130,15 +143,23 @@ const IndexPage = () =>
           <li>Tailwind CSS / Tachyons</li>
         </ul>
 
-        <p>Tools are just tools though. I'm open to work with anything exciting.</p>
+        <p>
+          Tools are just tools though. I'm open to work with anything exciting.
+        </p>
       </div>
     </div>
 
     <div className="pv2">
       <h4># Reach me</h4>
-      <p>I'm currently available for hire for contract/freelance projects! If you have a cool project and want to work together, contact me on Twitter (<a href="https://twitter.com/fishsander">@fishsander</a>) or send me an email at <a href="mailto:sander@sanderfish.nl">sander@sanderfish.nl</a>.</p>
+      <p>
+        I'm currently available for hire for contract/freelance projects! If you
+        have a cool project and want to work together, contact me on Twitter (<a href="https://twitter.com/fishsander">
+          @fishsander
+        </a>) or send me an email at{' '}
+        <a href="mailto:sander@sanderfish.nl">sander@sanderfish.nl</a>.
+      </p>
     </div>
-
   </div>
+);
 
-export default IndexPage
+export default IndexPage;
